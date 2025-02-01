87baef65c57e664ad3047dab43b378a9623a7d48
efasquel <erwan.fasquel@centralesupelec.fr>
2025-02-01 18:04:27 +0100

    Update user model to store password hash and unique email

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
+const argon2 = require("argon2");
+
+const emailRegex =
+  /^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const schema = new mongoose.Schema(
   {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [emailRegex, "Please provide a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       trim: true,
@@ -22,5 +36,24 @@ schema.method("toJSON", function toJSON() {
   object.id = _id;
   return object;
 });
+
+// Pre-save hook to hash the password
+schema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    try {
+      this.password = await argon2.hash(this.password);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
+
+// Method to compare passwords
+schema.methods.validatePassword = (candidatePassword) => {
+  return argon2.verify(this.password, candidatePassword);
+};
 
 module.exports = mongoose.model("User", schema);
